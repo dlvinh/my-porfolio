8a6faf7c1e3b006aed9383758ce21ab428912652
dlvinh <vinhxp95@gmail.com>
2022-10-31 18:43:46 +1100

    add blockquotes

--- src/Pages/AboutMe.jsx
+++ src/Pages/AboutMe.jsx
@@ -5,13 +5,17 @@ import style from "../Styles/AboutMe.module.css";
 export default function AboutMe() {
   return (
     <section className={`${style["about-me"]}`}>
-         <em><h3>About <span style={{color: "#ffd6a5"}}>Me!</span></h3></em> 
-      <article
-        className={`${style["about-me-article"]}`}>
+      <em>
+        <h3>
+          About <span style={{ color: "#ffd6a5" }}>Me!</span>
+        </h3>
+      </em>
+      <article className={`${style["about-me-article"]}`}>
         <div className={`${style["introduction"]}`}>
           <ul>
             <li>
-              I'm graduated <strong>Master of Information Technology</strong> in <strong>Monash University Australia</strong>
+              I'm graduated <strong>Master of Information Technology</strong> in{" "}
+              <strong>Monash University Australia</strong>
             </li>
             <li>
               My passion is Programming, and Web Developing I'm currently a
@@ -21,31 +25,85 @@ export default function AboutMe() {
           </ul>
         </div>
         <div className={`${style["personal-info"]}`}>
-          <p><span className="highlighted">Working at </span>Warehouse Assisstant - Toys R Us Anz Limited</p>
-          <p><span className= "highlighted">Age </span>28</p>
-          <p><span className= "highlighted">Address </span>Australia - Melbourne</p>
-          <p><span className= "highlighted">Email </span>vinhxp95@gmail.com</p>
-          <p><span className= "highlighted">Phone </span>0433986789</p>
-          <p><span className= "highlighted">Languages </span>Vietnamese - English</p>
+          <p>
+            <span className="highlighted">Working at </span>Warehouse Assisstant
+            - Toys R Us Anz Limited
+          </p>
+          <p>
+            <span className="highlighted">Age </span>28
+          </p>
+          <p>
+            <span className="highlighted">Address </span>Australia - Melbourne
+          </p>
+          <p>
+            <span className="highlighted">Email </span>vinhxp95@gmail.com
+          </p>
+          <p>
+            <span className="highlighted">Phone </span>0433986789
+          </p>
+          <p>
+            <span className="highlighted">Languages </span>Vietnamese - English
+          </p>
         </div>
       </article>
       <section className="skills mt-5 mb-3">
-        <em><h3>Skills</h3></em>
-        <div className={`${style["skills-container"]} d-flex mt-4 justify-content-around`}>
-          <img className={`${style['skill-logo']}`} alt="bootstrap" src='./assets/logos/icons8-bootstrap.svg'></img>
-          <img className={`${style['skill-logo']}`} alt="bootstrap" src='./assets/logos/icons8-css3.svg'></img>
-          <img className={`${style['skill-logo']}`} alt="bootstrap" src='./assets/logos/icons8-figma.svg'></img>
-          <img className={`${style['skill-logo']}`} alt="bootstrap" src='./assets/logos/icons8-html-5.svg'></img>
-          <img className={`${style['skill-logo']}`} alt="bootstrap" src='./assets/logos/icons8-javascript.svg'></img>
-          <img className={`${style['skill-logo']}`} alt="bootstrap" src='./assets/logos/icons8-react.svg'></img>
-          <img className={`${style['skill-logo']}`} alt="bootstrap" src='./assets/logos/icons8-redux.svg'></img>
+        <em>
+          <h3>Skills</h3>
+        </em>
+        <div
+          className={`${style["skills-container"]} d-flex mt-4 justify-content-around`}
+        >
+          <img
+            className={`${style["skill-logo"]}`}
+            alt="bootstrap"
+            src="./assets/logos/icons8-bootstrap.svg"
+          ></img>
+          <img
+            className={`${style["skill-logo"]}`}
+            alt="bootstrap"
+            src="./assets/logos/icons8-css3.svg"
+          ></img>
+          <img
+            className={`${style["skill-logo"]}`}
+            alt="bootstrap"
+            src="./assets/logos/icons8-figma.svg"
+          ></img>
+          <img
+            className={`${style["skill-logo"]}`}
+            alt="bootstrap"
+            src="./assets/logos/icons8-html-5.svg"
+          ></img>
+          <img
+            className={`${style["skill-logo"]}`}
+            alt="bootstrap"
+            src="./assets/logos/icons8-javascript.svg"
+          ></img>
+          <img
+            className={`${style["skill-logo"]}`}
+            alt="bootstrap"
+            src="./assets/logos/icons8-react.svg"
+          ></img>
+          <img
+            className={`${style["skill-logo"]}`}
+            alt="bootstrap"
+            src="./assets/logos/icons8-redux.svg"
+          ></img>
         </div>
       </section>
       <section className="done-task mt-5">
-        <em><h3>What I do</h3></em>
+        <em>
+          <h3>What I do</h3>
+        </em>
       </section>
       <DoneTask></DoneTask>
-     
+      <figure>
+        <blockquote class="blockquote">
+          <p>A well-known quote, contained in a blockquote element.</p>
+        </blockquote>
+        <figcaption class="blockquote-footer">
+          Someone famous in <cite title="Source Title">Source Title</cite>
+        </figcaption>
+      </figure>
     </section>
   );
 }
